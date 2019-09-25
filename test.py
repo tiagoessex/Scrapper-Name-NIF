af71c160ffda5ca9caacb4ad62a76355119e3838
arkhamlord666 <arkhamlord666@gmail.com>
2019-09-25 08:48:11 +0100

    little adjustment

--- test.py
+++ test.py
@@ -6,9 +6,9 @@ s = scraping.Scrapping()
 
 try:
 	# randomize the order of the services
-	r = s.scrap("Dom Digital - Novas Tecnologias De Informação, Lda.", True)
+	r = s.scrap("Dom Digital - Novas Tecnologias De Informação, Lda.")
 	# don't randomize the order of the services
-	r = s.scrap("Dom Digital - Novas Tecnologias De Informação, Lda.", False)
+	r = s.scrap("Dom Digital - Novas Tecnologias De Informação, Lda.", randomize = True)
 	print (r)
 except Exception as e:
 	print (e)
